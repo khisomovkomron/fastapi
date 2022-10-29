feeb26df1de51e964321c4036b9e85a20ec1b1b3
k.hisomov <normok_9595@bk.ru>
2022-10-29 14:28:20 +0300

    jwt setup - login/ hashed password

--- TodoApp/auth.py
+++ TodoApp/auth.py
@@ -1,7 +1,17 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends, HTTPException
 from pydantic import BaseModel
 from typing import Optional
 import models
+from passlib.context import CryptContext
+from sqlalchemy.orm import Session
+from database import SessionLocal, engine
+from fastapi.security import OAuth2PasswordRequestForm, OAuth2PasswordBearer
+from datetime import datetime, timedelta
+from jose import jwt
+
+
+SECRET_KEY = 'freufghjioWJIOdw123142'
+ALGORITHM = 'HS256'
 
 
 class CreateUser(BaseModel):
@@ -12,17 +22,77 @@ class CreateUser(BaseModel):
     password: str
 
 
+bcrypt_context = CryptContext(schemes=['bcrypt'], deprecated=['auto'])
+
+models.Base.metadata.create_all(bind=engine)
+
+oauth2_bearer = OAuth2PasswordBearer(tokenUrl='token')
+
 app = FastAPI()
 
 
+def get_db():
+    try:
+        db = SessionLocal()
+        yield db
+    finally:
+        db.close()
+
+
+def get_password_hash(password):
+    return bcrypt_context.hash(password)
+
+
+def verify_password(plain_password, hashed_password):
+    return bcrypt_context.verify(plain_password, hashed_password)
+
+
+def authenticate_user(username: str, password: str, db):
+    user = db.query(models.Users).filter(models.Users.username == username).first()
+    
+    if not user:
+        return False
+    if not verify_password(password, user.hashed_password):
+        return False
+    return user
+
+
+def create_access_token(username: str, user_id: int, expired_delta: Optional[timedelta] = None):
+    encode = {'sub': username, 'id': user_id}
+    if expired_delta:
+        expire = datetime.utcnow() + expired_delta
+    else:
+        expire = datetime.utcnow() + timedelta(minutes=15)
+    encode.update({'exp': expire})
+    return jwt.encode(claims=encode, key=SECRET_KEY, algorithm=ALGORITHM)
+    
+
+
 @app.post('/create/user')
-async def create_new_user(create_user: CreateUser):
+async def create_new_user(create_user: CreateUser, db: Session = Depends(get_db)):
     create_user_model = models.Users()
     create_user_model.email = create_user.email
     create_user_model.username = create_user.username
     create_user_model.first_name = create_user.first_name
     create_user_model.last_name = create_user.last_name
-    create_user_model.hashed_password = create_user.password
+    
+    hash_password = get_password_hash(create_user.password)
+    create_user_model.hashed_password = hash_password
     create_user_model.is_active = True
     
-    return create_user_model
+    db.add(create_user_model)
+    db.commit()
+    
+
+@app.post('/token')
+async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends(),
+                                 db: Session = Depends(get_db)):
+    user = authenticate_user(form_data.username, form_data.password, db)
+    if not user:
+        raise HTTPException(status_code=404, detail='User not found')
+    token_expires = timedelta(minutes=15)
+    
+    token = create_access_token(user.username,
+                                user.id,
+                                expired_delta=token_expires)
+    return {'token': token}
