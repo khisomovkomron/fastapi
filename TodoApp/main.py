aa445d9bcc1e2ff8efa2525b15afea308fa96e74
k.hisomov <normok_9595@bk.ru>
2022-10-31 23:46:18 +0300

    routing dependencies

--- TodoApp/main.py
+++ TodoApp/main.py
@@ -1,8 +1,8 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
 import models
 from database import engine
 from routers import auth, todos
-from company import companyapis
+from company import companyapis, dependencies
 
 app = FastAPI()
 
@@ -14,5 +14,6 @@ app.include_router(
     router=companyapis.router,
     prefix='/companyapis',
     tags=['companyapis'],
+    dependencies=[Depends(dependencies.get_token_header)],
     responses={418: {"description": "Internal Use Only"}}
 )
